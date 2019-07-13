395f41b7ea6404b1fc5d6b0a0cd3224520fa387b
Ander Plascencia <anderplascencia@gmail.com>
2019-07-13 13:26:23 -0500

    variables de entorno

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y#radf-90pkj!fc1!r@lot#z6-t%w5@r5fmyp=n^*mm8$dk(14'
+# SECRET_KEY = 'y#radf-90pkj!fc1!r@lot#z6-t%w5@r5fmyp=n^*mm8$dk(14'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
